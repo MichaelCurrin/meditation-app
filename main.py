bce2079f0c66dfe96b9ea52cfe88ec7df927a075
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-11-16 21:49:24 +0100

    feat: create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+
+import pyttsx3
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "")  # e.g. 'gpt-4o'
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "local")
+OPENAI_API_URL = os.getenv("OPENAI_API_URL", "http://localhost:1234/v1")
+
+
+def generate(model, user_input):
+    prompt_template = PromptTemplate.from_template(
+        "Create a calming meditation experience to guide the user through a calm and detailed scene. If it is a story, describe what happens in the story, if not a story then describe observations from the user perspective. Use the following input: {input}"
+    )
+    prompt = prompt_template.format(input=user_input)
+    result = model.invoke(prompt)
+    return result.content
+
+
+def main():
+    model = ChatOpenAI(
+        model=OPENAI_MODEL,
+        base_url=OPENAI_API_URL,
+        api_key=OPENAI_API_URL,
+        max_tokens=100,
+    )
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 150)
+
+    result = generate(
+        model, "I walk through a misty forest on a mountain in the evening"
+    )
+    engine.say(result)
+    engine.runAndWait()
+
+
+if __name__ == "__main__":
+    main()
